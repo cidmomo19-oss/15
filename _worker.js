bb0ac16db01fd2165b02ccd2343df62c708cfb6e
Okienak <cidmomo19@gmail.com>
2026-06-27 16:14:50 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -9,7 +9,7 @@ export default {
     try {
       const videoResponse = await fetch(videoUrl, {
         headers: {
-          'Referer': 'https://pixeldrain.com/', // <- PERHATIKAN SLASH DI AKHIR
+          'Referer': 'https://pixeldrain.com/u/', // <- PERHATIKAN SLASH DI AKHIR
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
           'Accept': '*/*', // <- TAMBAHKAN INI
           'Accept-Language': 'en-US,en;q=0.9',
